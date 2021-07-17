68ea14738e550ec59c062c7e7ab22613ce18d569
beaever <thebeaever@gmail.com>
2021-07-18 01:52:59 +0900

    0718_addEventListener , hasgchange 기능 추가

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+const container = document.getElementById('root');
 const ajax =  new XMLHttpRequest();
+const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+
+
 ajax.open('GET', NEWS_URL , false);  // 동기화 처리 하겠다.
 ajax.send();
 
@@ -7,10 +12,32 @@ ajax.send();
 const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement('ul');
 
+window.addEventListener('hashchange', function () {
+  const id = location.hash.substr(1);
+
+  ajax.open('GET', CONTENT_URL.replace('@id',id), false);
+  ajax.send();
+
+  const newsContent = JSON.parse(ajax.response);
+  const title = document.createElement('h1');
+
+  title.innerHTML = newsContent.title;
+
+  content.appendChild(title);  
+});
+
 for (let i = 0; i < 10; i++) {
   const li = document.createElement('li');
-  li.innerHTML = newsFeed[i].title;
-   ul.appendChild(li);
+  const a = document.createElement('a');
+
+  a.href = `#${newsFeed[i].id}`;
+  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+  
+
+
+  ul.appendChild(li);
+   li.appendChild(a);
 }
 
-document.getElementById('root').appendChild(ul);
+container.appendChild(ul);
+container.appendChild(content);
